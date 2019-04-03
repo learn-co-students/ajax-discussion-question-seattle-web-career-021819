9b59e63119f4b6336d33a0d56c9cc04aee02b6ea
github username <github email address>
2019-04-03 10:20:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  const button = document.getElementById("button");
+  const name = document.getElementById("fullname");
+  const phonenum = document.getElementById("phone");
+  const cellnum = document.getElementById("cell");
+  const postcode = document.getElementById("postcode");
+  const city = document.getElementById("city");
+  const state = document.getElementById("state");
+  const street = document.getElementById("street");
+  const email = document.getElementById("email");
+  const dob = document.getElementById("date_of_birth");
+
+  function fetchData(){
+         var data = fetch('https://randomuser.me/api/')
+           .then( res => res.json() )
+           .then( data => {populatePage(data); console.log(data) })
+
+  }
+
+  function populatePage(data){
+    personData = data.results[0];
+    name.textContent = (personData.name.first.charAt(0).toUpperCase() + personData.name.first.slice(1) + " "+ personData.name.last.charAt(0).toUpperCase() + personData.name.last.slice(1));
+    phonenum.textContent = personData.phone;
+    cellnum.textContent = personData.cell;
+    email.textContent = personData.email;
+    dob.textContent = personData["dob"].date.split("T")[0];
+    street.textContent = personData.location.street;
+    city.textContent = personData.location.city;
+    state.textContent = personData.location.state;
+    postcode.textContent = personData.location.postcode;
+  }
+
+ button.addEventListener("click", ()=> {
+   fetchData();
+ });
+ // function(ev) }
+ // }
+ //
+ // (ev) => {
+ //
+ //  }
+
+  const fullname = document.getElementById("fullname");
+  console.log("CONTENT NOT YET LOADED!", fullname);
+
 });
